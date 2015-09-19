ae8288d9716cf47e4aa597cea4ab881116ba3848
Yifei LIU <yfliu87@gmail.com>
2015-09-19 20:39:39 +0800

    replace lda model with lsi model

--- task3/task3.1.py
+++ task3/task3.1.py
@@ -5,7 +5,7 @@ base_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_da
 business_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
 business_review_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
 manual_annotation_task_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task3/manualAnnotationTask'
-
+validate_output_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task3/validateOutput'
 
 def get_target_business_type():
 	result = []
@@ -125,29 +125,29 @@ def preprocess(reviews):
 	from nltk.tokenize import word_tokenize
 
 	review_tokenized = [[word.lower() for word in word_tokenize(review.decode('utf-8'))] for review in reviews] 
-	print "review tokenize done"
+	#print "review tokenize done"
 
 	#remove stop words
 	from nltk.corpus import stopwords
 	english_stopwords = stopwords.words('english')
 	review_filterd_stopwords = [[word for word in review if not word in english_stopwords] for review in review_tokenized]
-	print 'remove stop words done'
+	#print 'remove stop words done'
 
 	#remove punctuations
 	english_punctuations = [',','.',':',';','?','(',')','&','!','@','#','$','%']
 	review_filtered = [[word for word in review if not word in english_punctuations] for review in review_filterd_stopwords]
-	print 'remove punctuations done'
+	#print 'remove punctuations done'
 
 	#stemming
 	from nltk.stem.lancaster import LancasterStemmer
 	st = LancasterStemmer()
 	review_stemmed = [[st.stem(word) for word in review] for review in review_filtered]
-	print 'stemming done'
+	#print 'stemming done'
 
 	return review_stemmed
 
 
-def train_by_lda(reviews):
+def train_by_lsi(reviews):
 	from gensim import corpora, models, similarities
 
 	dictionary = corpora.Dictionary(reviews)
@@ -155,23 +155,24 @@ def train_by_lda(reviews):
 	tfidf = models.TfidfModel(corpus)
 	corpus_tfidf = tfidf[corpus]
 
-	lda = models.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=10)
-	index = similarities.MatrixSimilarity(lda[corpus])
+	lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=10)
+	index = similarities.MatrixSimilarity(lsi[corpus])
 
-	print "train by lda done"
-	return (index, dictionary, lda)
+	print "train by lsi done"
+	return (index, dictionary, lsi)
+
 
 
 if __name__ == '__main__':
+	'''
 	target_business_type = get_target_business_type()
 	business_category_id_map = build_business_type_id_map(target_business_type)
 	business_id_review_map = read_review_related_to_business_id(business_category_id_map)
 	output_category_review_to_disk(business_category_id_map, business_id_review_map)
-
+	'''
 	reviews = read_review('Chinese')
 	reviews_processed =	preprocess(reviews)
+	(lsi_index, lsi_dictionary, lsi) = train_by_lsi(reviews_processed)	
 
-	(lda_index, lda_dictionary, lda) = train_by_lda(reviews_processed)	
-	#validate_dishes(validate_output_file_path, lda_index, lda_dictionary, lda)
 	#getSimOfAllReviews(lsiOutputPath, fileList, lsi_index, lsi_dictionary, lsi)
 	#generateSimilarityMap(lsiOutputPath, 'LSI_TFIDF')
