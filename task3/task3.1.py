245517699157856c5b7e9dbf08d69f8d4c9cb3a3
Yifei LIU <yfliu87@gmail.com>
2015-09-20 10:52:08 +0800

    read tips for business category

--- task3/task3.1.py
+++ task3/task3.1.py
@@ -4,6 +4,7 @@ import json
 base_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task3'
 business_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
 business_review_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_review.json'
+business_tip_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_tip.json'
 manual_annotation_task_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task3/manualAnnotationTask'
 validate_output_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task3/validateOutput'
 
@@ -57,6 +58,13 @@ def record_review(business_id_review_map, bus_id, review):
 	business_id_review_map[bus_id].append(review)
 
 
+def record_tip(business_id_tip_map, bus_id, tip):
+	if bus_id not in business_id_tip_map:
+		business_id_tip_map[bus_id] = []
+
+	business_id_tip_map[bus_id].append(tip)
+
+
 def read_review_related_to_business_id(business_category_id_map):
 	reader = open(business_review_file_path, 'r')
 	business_id_review_map = {}
@@ -76,7 +84,26 @@ def read_review_related_to_business_id(business_category_id_map):
 	return business_id_review_map 
 
 
-def output_category_review_to_disk(business_category_id_map, business_id_review_map):
+def read_tip_related_to_business_id(business_category_id_map):
+	reader = open(business_tip_file_path, 'r')
+	business_id_tip_map = {}
+
+	line = reader.readline()
+
+	while line:
+		json_line = json.loads(line)
+		bus_id = json_line['business_id'] 
+
+		if included_in_target_business_type(bus_id, business_category_id_map):
+			record_tip(business_id_tip_map, bus_id, json_line['text'])
+
+		line = reader.readline()
+
+	reader.close()
+	return business_id_tip_map 
+
+
+def output_category_review_to_disk(business_category_id_map, business_id_review_map, business_id_tip_map):
 	import codecs
 
 	for cat in business_category_id_map:
@@ -84,12 +111,14 @@ def output_category_review_to_disk(business_category_id_map, business_id_review_
 		business_ids = business_category_id_map[cat]
 
 		for bus_id in business_ids:
-			if bus_id not in business_id_review_map:
-				continue 
+			if bus_id in business_id_review_map: 
+				reviews += business_id_review_map[bus_id]
 
-			reviews += business_id_review_map[bus_id]
+			if bus_id in business_id_tip_map:
+				reviews += business_id_tip_map[bus_id]
 
-		file_name = base_path + '/categoryReviewMap/' + cat + '_reviews.txt'
+
+		file_name = base_path + '/categoryReviewMap/' + cat + '_tip_reviews.txt'
 
 		with codecs.open(file_name, 'a', encoding='utf-8') as writer:
 			writer.write(''.join(reviews))
@@ -219,14 +248,16 @@ def output_to_disk(output_file_path, dish_appearance):
 
 
 if __name__ == '__main__':
-	'''
 	target_business_type = get_target_business_type()
 	business_category_id_map = build_business_type_id_map(target_business_type)
 	business_id_review_map = read_review_related_to_business_id(business_category_id_map)
-	output_category_review_to_disk(business_category_id_map, business_id_review_map)
+	business_id_tip_map = read_tip_related_to_business_id(business_category_id_map)
+	output_category_review_to_disk(business_category_id_map, business_id_review_map, business_id_tip_map)
+	
 	'''
 	reviews = read_review('Chinese')
 	reviews_processed =	preprocess(reviews)
 	(lsi_index, lsi_dictionary, lsi) = train_by_lsi(reviews_processed)	
 
 	validate_dishes('Chinese', reviews, validate_output_file_path, lsi_index, lsi_dictionary, lsi)
+	'''
