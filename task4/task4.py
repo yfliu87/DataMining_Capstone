7fccdd7feb2bcfae8b02ec17d9698fcbde05ae63
Yifei LIU <yfliu87@gmail.com>
2015-09-23 21:07:14 +0800

    read dishes from given file

--- task4/task4.py
+++ task4/task4.py
@@ -1,4 +1,5 @@
 business_file_path = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/yelp_academic_dataset_business.json'
+chinese_dish_file = '/home/yfliu/DataMining_Workspace/DataMining/CapstoneProject/yelp_dataset_challenge_academic_dataset/task4/student_dn_annotations.txt'
 
 def build_business_type_restaurant_id_map(target_type):
 	import json
@@ -23,5 +24,23 @@ def build_business_type_restaurant_id_map(target_type):
 	return result
 
 
+def read_chinese_dish_from_file(dish_file):
+	dishes = set() 
+
+	reader = open(dish_file, 'r')
+	line = reader.readline()
+
+	while line:
+		if line != '':
+			dishes.add(line.replace('\n', ''))
+
+		line = reader.readline()
+
+	reader.close()
+
+	return list(dishes)
+
+
 if __name__ == '__main__':
-	build_business_type_restaurant_id_map("Chinese")
+	chinese_restaurants = build_business_type_restaurant_id_map("Chinese")
+	dishes = read_chinese_dish_from_file(chinese_dish_file)
